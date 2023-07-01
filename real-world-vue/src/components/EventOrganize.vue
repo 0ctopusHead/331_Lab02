eaf224ca3b8c5d19f3bff73e60fbec000744d659
Yanwarut Suksawat <yanwarut_s@cmu.ac.th>
2023-07-01 17:43:55 +0700

    4.15*

---
+++ real-world-vue/src/components/EventOrganize.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="organize-class">
+   <div class="organize-card">
+    <span>
+       Category : {{ event?.category }}
+    </span> 
+     <br/>
+    <span>
+       Organizer : {{ event?.organizer }}
+    </span>
+  
+   
+   </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import type { EventItem } from '@/type'
+  import type { PropType } from 'vue' 
+  
+const props = defineProps({
+  event: {
+    type: Object as PropType<EventItem>,
+    require: true
+  }
+})
+</script>
+
+<style>
+.organize-card {
+  padding: 20px;
+  width: 250px;
+  margin-bottom: 18px;
+  text-align: right;
+}
+
+</style>
