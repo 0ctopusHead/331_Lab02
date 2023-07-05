8acb2469b97e5836773b740f98300049ca38298f
Yanwarut Suksawat <yanwarut_s@cmu.ac.th>
2023-07-05 23:54:10 +0700

    fixed

--- real-world-vue/src/views/event/EventLayoutView.vue
+++ real-world-vue/src/views/event/EventLayoutView.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { type EventItem } from '@/type';
+import { useRouter } from 'vue-router';
 import EventService from '@/services/EventService';
 const event = ref<EventItem | null>(null)
 
 const props = defineProps({
     id: String
 })
+const router = useRouter()
 
 EventService.getEventById(Number(props.id))
    .then((respond) => {
@@ -14,11 +16,17 @@ EventService.getEventById(Number(props.id))
    })
    .catch((error) =>{
     console.log(error)
+    if(error.respond && error.respond.status === 404){
+        router.push({ name: '404:resource', params: {resource : 'event'}})
+    }
+    else{
+        router.push({name: 'network-error'})
+    }
    })
 </script>
 <template>
     <div v-if="event">
-        <h1>{{ event.title }}</h1>
+        <h1>{{ event.title }} </h1>
         <div id="nav">
             <router-link :to="{name: 'event-detail', params: {id}}">Details</router-link>
             |
@@ -29,3 +37,15 @@ EventService.getEventById(Number(props.id))
         <RouterView :event="event"></RouterView>
     </div>
 </template>
+<style>
+nav {
+  padding: 30px;
+}
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
