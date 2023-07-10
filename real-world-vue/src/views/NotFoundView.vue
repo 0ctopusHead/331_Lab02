3333629eb18eec86940d707e14bd31a6fd04f6a0
Yanwarut Suksawat <yanwarut_s@cmu.ac.th>
2023-07-10 19:52:34 +0700

    Fixed*****

--- real-world-vue/src/views/NotFoundView.vue
+++ real-world-vue/src/views/NotFoundView.vue
@@ -8,7 +8,9 @@ defineProps({
 })
 </script>
 <template>
-    <h1>Oops!</h1>
+   <div class="flex flex-col items-center">
+    <h1 class="text-3xl">Oops!</h1>
     <h3>The {{ resource }} you looking for is not here</h3>
-    <RouterLink :to="{name: 'event-list'}">Back to home page</RouterLink>
+    <RouterLink class="underline text-blue-700" :to="{name: 'event-list'}">Back to home page</RouterLink>
+    </div>
 </template>
